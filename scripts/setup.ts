ad0d0330b0db0e56d598c9b66f79df1f9750a288
Tom Golden <github@tbjgolden.com>
2021-01-30 14:18:04 -0600

    chore: last commit before the storm

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -114,6 +114,16 @@ const setup = async () => {
       ) {
         return 'github username is invalid'
       }
+      if (values.authorName.length === 0) {
+        return 'author name is invalid'
+      }
+      if (
+        !/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/.test(
+          values.authorEmail
+        )
+      ) {
+        return 'author email is invalid'
+      }
       return true
     }) as any
   })
