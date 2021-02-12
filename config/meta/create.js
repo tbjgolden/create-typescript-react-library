e73b55f0c8cf2de66fe10eb03a03b06b55af1c35
Tom Golden <8162045+tbjgolden@users.noreply.github.com>
2021-02-11 20:13:03 -0600

    fix: bug with setup script, increment version

--- config/meta/create.js
+++ config/meta/create.js
@@ -43,17 +43,21 @@ const run = async () => {
     `git clone --depth 1 https://github.com/tbjgolden/create-typescript-react-library.git '${answer}'`
   )
   execSync(`yarn`, { cwd: newDir })
+  execSync(`rm -rf .git`, { cwd: newDir })
+  execSync(`git init`, { cwd: newDir })
 
-  const installer = spawn(
-    './node_modules/.bin/ts-node',
-    ['./scripts/setup.ts'],
-    {
-      cwd: newDir,
-      stdio: 'inherit'
-    }
-  )
+  const installer = spawn('yarn', ['ctrl', 'setup'], {
+    cwd: newDir,
+    stdio: 'inherit'
+  })
 
   installer.on('close', (code) => {
+    console.log(
+      `Change to your new library directory:\n  cd ${path.relative(
+        process.cwd(),
+        newDir
+      )}`
+    )
     process.exit(code)
   })
 }
